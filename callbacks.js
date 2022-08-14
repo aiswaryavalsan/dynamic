1d43b24cb5b08f5372b0916db8f682a6c55486a1
aiswarya <aiswaryavalsan@gmail.com>
2022-08-14 09:12:13 -0500

    promise

--- callbacks.js
+++ callbacks.js
@@ -1,36 +1,34 @@
-const post=[{title:'post1',body:'this is post1',createdAt:new Date().getTime()},{title:'post2',body:'this is post2',createdAt:new Date().getTime()}];
-let intervalid=0;
+const post=[{title:'post1',body:'this is post1',createdAt:new Date().getTime()},{title:'post2',body:'this is post2'}];
+
 function getPosts(){
-    clearInterval(intervalid);
-    intervalid=setInterval(()=>{
+   
+    setTimeout(()=>{
        let output='';
         post.forEach((p)=>{
-            output+=`<li>${p.title},-last updated${(new Date().getTime()-p.createdAt)/1000}seconds ago</li>`;
+            output+=`<li>${p.title}</li>`;
         });
         document.body.innerHTML=output;
         //console.log(post);
     },1000);
 }
-function createPost(p,callback){
-    setTimeout(()=>{
-        post.push({...p, createdAt:new Date().getTime()});
-        callback();
+function createPost(p){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        post.push(p);
+        const error=false;
+        if(!error)
+        {
+            resolve();
+        }
+        else{
+            reject('something went wrong');
+        }
     },2000);
-}
-function create4thpost(p,callback){
-    setTimeout(()=>{
-        post.push({...p,createdAt:new Date().getTime()});
-       callback();
 
-    },2000);
-
+    });
 }
 
-
-
-
-createPost({title:'post3',body:'this is post3'},getPosts);
-create4thpost({title:'post4',body:'this is post4'},createPost);
+createPost({title:'post3',body:'this is post3'}).then(getPosts);
 
 console.log(post);
 //getPosts();
